e94ad2ba2548849503f6dc7718536eb5f738e21f
Jeffrey Bailey <bailey.jeffrey@yahoo.com>
2020-01-14 05:49:23 -0700

    adding database connection

--- server.js
+++ server.js
@@ -10,10 +10,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-acute-84559',
-    user : 'postgres',
-    password : 'OneFineDay##1234',
-    database : 'smartbraindb'
+    host : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
